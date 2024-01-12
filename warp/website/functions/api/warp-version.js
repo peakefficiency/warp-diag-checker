8aca33dad74cb1880ebc584f33fde44a3ae8364a
Erich Simon <eprosimon@gmail.com>
2024-01-12 12:54:23 +0000

    change error for debugging

--- warp/website/functions/api/warp-version.js
+++ warp/website/functions/api/warp-version.js
@@ -4,7 +4,7 @@ export async function onRequestGet(context) {
     const url = new URL(request.url);
     const endpoint = url.pathname.split('/').pop();
 
-    if (endpoint === 'linux') {
+    if (endpoint === '/linux') {
         try {
             // Replace 'MY_KV_NAMESPACE' with your actual KV namespace binding name
             const data = await env.WARP_DIAG_CHECKER.get('linux-version');
@@ -13,6 +13,6 @@ export async function onRequestGet(context) {
             return new Response('Error fetching data', { status: 500 });
         }
     } else {
-        return new Response('Not found', { status: 404 });
+        return new Response('Not found', { status: 401 });
     }
 }
